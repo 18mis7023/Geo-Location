0b1b59674246e505df10f7a1d683c5f2984850ee
Sudhee-bsp <sudheendrapai.b@gmail.com>
2021-12-11 13:57:35 +0530

    map display

--- src/App.js
+++ src/App.js
@@ -6,14 +6,36 @@ const App = () => {
   const [status, setStatus] = useState(null);
   //   const [status, setStatus] = useState([]);
 
-  // const [link, setLink] = useState(null);
-  const [link, setLink] = useState([]);
+  const [link, setLink] = useState(null);
+  // const [link, setLink] = useState([]);
 
-  // const getLocation = () => {
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.watchPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+          setLink(
+            `https://www.google.com/maps/embed/v1/place?key=AIzaSyAQCY4YwtRmH9fFo77HXc4YA-4jANEsMGE&q=${lat},${lng}`
+          );
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  // useEffect(() => {
   //   if (!navigator.geolocation) {
   //     setStatus("Geolocation is not supported by your browser");
   //   } else {
   //     setStatus("Locating...");
+  //     //   navigator.geolocation.getCurrentPosition(
   //     navigator.geolocation.watchPosition(
   //       (position) => {
   //         setStatus(null);
@@ -26,47 +48,47 @@ const App = () => {
   //       }
   //     );
   //   }
-  // };
+  // }, []);
 
-  useEffect(() => {
-    if (!navigator.geolocation) {
-      setStatus("Geolocation is not supported by your browser");
-    } else {
-      setStatus("Locating...");
-      //   navigator.geolocation.getCurrentPosition(
-      navigator.geolocation.watchPosition(
-        (position) => {
-          setStatus(null);
-          setLat(position.coords.latitude);
-          setLng(position.coords.longitude);
-          setLink("http://www.google.com/maps/place/" + lat + "," + lng);
-        },
-        () => {
-          setStatus("Unable to retrieve your location");
-        }
-      );
-    }
-  }, []);
+  // show iframe only if lat and lng are available
+  let showMap =
+    lat && lng ? (
+      <div>
+        <h4>Here's your location</h4>
+        <iframe
+          width="800"
+          height="470"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAQCY4YwtRmH9fFo77HXc4YA-4jANEsMGE&q=${lat},${lng}`}
+        ></iframe>
+      </div>
+    ) : null;
 
   return (
     <div className="App">
-      {/* <button onClick={getLocation}>START</button> */}
-      <button>START</button>
+      <button onClick={getLocation}>START</button>
+      {/* <button>START</button> */}
       <h1>Coordinates</h1>
       <p>{status}</p>
       {lat && <p>Latitude: {lat}</p>}
       {lng && <p>Longitude: {lng}</p>}
       <br></br>
-      <iframe
+
+      {/* <iframe
         width="800"
         height="470"
         frameborder="0"
         scrolling="no"
         marginheight="0"
         marginwidth="0"
-        // src="https://maps.google.com/maps?q=""&hl=es&z=14&amp;output=embed"
-        src={link}
-      ></iframe>
+        // src="https://maps.google.com/maps?q=16.3080284,80.4397923&hl=eng&z=14&amp;output=embed"
+        // src={`https://maps.google.com/maps?q=${lat},${lng}&hl=eng&z=14&amp;output=embed`}
+        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAQCY4YwtRmH9fFo77HXc4YA-4jANEsMGE&q=${lat},${lng}`}
+      ></iframe> */}
+      {showMap}
     </div>
   );
 };
